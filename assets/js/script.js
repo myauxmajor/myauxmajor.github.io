4eb3f965d9434db0509765d00424055e2d31c7a2
myauxmajor <myaux@myauxmajor.com>
2025-03-17 14:13:45 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    alert("This page is under contruction!");
-});
-*/
 
 function flipCard(card) {
     card.classList.toggle("flipped");
